3e7f0147636a2884554adc3a8b68446e0e1a57b0
mbp1225 <matheus698@hotmail.com>
2018-05-19 00:12:56 -0300

    Initial commit

---
+++ Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController controller;
+
+    [SerializeField] float speed;
+    [SerializeField] float lookSpeed;
+
+    float movH, movV;
+    float mouseX, mouseY;
+
+	void Start ()
+    {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	void Update ()
+    {
+        movV = Input.GetAxis("Vertical");
+        movH = Input.GetAxis("Horizontal");
+
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        if (movV > 0.1f || -0.1f > movV || movH > 0.1f || -0.1f > movH) controller.SimpleMove(transform.forward * speed * movV + transform.right * speed * movH);
+
+        if (mouseX > 0.1f || -0.1f > mouseX) transform.eulerAngles += new Vector3(0, mouseX * lookSpeed * Time.deltaTime, 0);
+    }
+}
